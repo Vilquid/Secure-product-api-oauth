1f9e06661848d3b56e9746a0185c49d5b749b497
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-03-11 10:57:59 +0700

    Add Jwt decoder method to the security config class

--- product/src/main/java/ku/product/config/SecurityConfig.java
+++ product/src/main/java/ku/product/config/SecurityConfig.java
@@ -1,15 +1,25 @@
 package ku.product.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
+	@Value("${auth0.audience}")
+	private String audience;
+
+	@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+	private String issuer;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
@@ -27,6 +37,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 			// use oauth as a resource server to do jwt validation
 		.and()
 				.oauth2ResourceServer()
-				.jwt();
+				.jwt()
+				.decoder(jwtDecoder());
+	}
+
+	private JwtDecoder jwtDecoder()
+	{
+		OAuth2TokenValidator<Jwt> withAudience = new AudienceValidator(audience);
+		OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+		OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withAudience, withIssuer);
+		NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)JwtDecoders.fromOidcIssuerLocation(issuer);
+		jwtDecoder.setJwtValidator(validator);
+
+		return jwtDecoder;
 	}
 }
