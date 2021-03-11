f9098d52950d9bc3dcdd9787a31ba85809c2aaa3
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-03-11 15:26:14 +0700

    Add getters/setters

--- product/src/main/java/ku/product/model/Product.java
+++ product/src/main/java/ku/product/model/Product.java
@@ -15,9 +15,36 @@ public class Product
 	private String name;
 	private double price;
 
+	public long getId()
+	{
+		return id;
+	}
+
+	public void setId(long id)
+	{
+		this.id = id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public double getPrice()
+	{
+		return price;
+	}
+
+	public void setPrice(double price)
+	{
+		this.price = price;
+	}
+
 	public Product()
 	{}
-
-	// ... generate all getters / setters
-
 }
