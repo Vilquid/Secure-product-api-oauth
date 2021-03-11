daa4632bee48df3e2500956eb9d2bbe57d675ed4
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-03-11 10:51:38 +0700

    Change to SecurityConfig to force authentication via jwt using oauth resource server which will help validating the jwt token

--- product/src/main/java/ku/product/config/SecurityConfig.java
+++ product/src/main/java/ku/product/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -15,8 +16,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 		http
 				.csrf().disable()
 				.authorizeRequests()
-				.antMatchers("/api/products").permitAll()
 				.anyRequest()
 				.authenticated();
+
+		// use stateless session, so user's state is not stored
+		.and()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+			// use oauth as a resource server to do jwt validation
+		.and()
+				.oauth2ResourceServer()
+				.jwt();
 	}
 }
