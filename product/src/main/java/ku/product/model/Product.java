8f5a79a9b59a023bce493d587d57f391b7d427cc
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-03-11 10:14:23 +0700

    Add Product class
    Create model package
    Create repository package
    Create service package
    Create controller package

---
+++ product/src/main/java/ku/product/model/Product.java
@@ -0,0 +1,23 @@
+package ku.product.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String name;
+	private double price;
+
+	public Product()
+	{}
+
+	// ... generate all getters / setters
+
+}
